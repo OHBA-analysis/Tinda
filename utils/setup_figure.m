c1cf33bbb2b3b8bc48d437043e45077ea22fef4b
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-09-29 17:19:47 +0100

    first commit

---
+++ utils/setup_figure.m
@@ -0,0 +1,31 @@
+function fig = setup_figure(fig,width,hw_ratio,box)
+if ~exist('fig', 'var') || isempty(fig)
+  fig=figure;
+end
+if ~exist('width', 'var') || isempty(width)
+  width=2; % columns
+end
+if ~exist('hw_ratio', 'var') || isempty(hw_ratio)
+  hw_ratio=0.5; 
+end
+if ~exist('box', 'var') || isempty(box)
+  box='off'; 
+end
+
+% Elsevier recommendations
+if width==1
+  picturewidth = 9; 
+elseif width==1.5
+  picturewidth = 14; 
+elseif width==2
+  picturewidth = 19; 
+end
+set(findall(fig,'-property','FontSize'),'FontSize',14) % adjust fontsize to your document
+set(findall(fig,'-property','FontName'),'FontName','Calibri')
+
+set(findall(fig,'-property','Box'),'Box',box) % optional
+set(findall(fig,'-property','Interpreter'),'Interpreter','latex') 
+set(findall(fig,'-property','TickLabelInterpreter'),'TickLabelInterpreter','latex')
+set(fig,'Units','centimeters','Position',[3 3 picturewidth hw_ratio*picturewidth])
+pos = get(fig,'Position');
+set(fig,'PaperPositionMode','Auto','PaperUnits','centimeters','PaperSize',[pos(3), pos(4)])
