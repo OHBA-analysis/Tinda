62a251b6c07ce03280bf515b55fcf092f486e65c
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-11-28 11:02:40 +0000

    add documentation

--- utils/compute_rotational_momentum.m
+++ utils/compute_rotational_momentum.m
@@ -1,6 +1,9 @@
 function rotational_momentum = compute_rotational_momentum(angleplot, FO_assym, whichstate)
 tmp = angleplot.*FO_assym;
-if exist('whichstate','var') && ~isempty(whichstate) 
+if exist('whichstate','var') && ~isempty(whichstate)
+  % Note that we are counting each (i,j) double because for the rotational
+  % momentum per state we take into account (i,j) and (j,i) for all j and one
+  % particular i.
   tmp = squeeze(tmp(whichstate,:,:)) + squeeze(tmp(:,whichstate,:));
   rotational_momentum = imag(sum(tmp));
 else
