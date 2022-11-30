181b31429c7bac7fd45b148bb2ee95c7d0e4522d
matsvanes <mats.vanes@psych.ox.ac.uk>
2022-11-30 16:35:14 +0000

    make colormap optional

--- utils/cyclicalstate_distributionplot.m
+++ utils/cyclicalstate_distributionplot.m
@@ -1,14 +1,17 @@
-function fig = cyclicalstate_distributionplot(ordering, weights, CL)
+function fig = cyclicalstate_distributionplot(ordering, weights, CL, CM)
 
 % edges = logspace(log10(CL(1)), log10(CL(2)), 100);
 edges = linspace(CL(1), 1.1*CL(2), 100);
 color_ix = discretize(weights, edges);
 
-CM = colormap(inferno(120));
-CM = CM(20:end,:);
+if ~exist('CM', 'var') || isempty(CM)
+  CM = colormap(inferno(120));
+  CM = CM(20:end,:);
+end
 for k=1:12
   color_scheme{k} = CM(color_ix(k),:);
 end
 cyclicalstatesubplot(ordering, zeros(12), zeros(12), color_scheme)
 hold on
-spider_plot(color_ix(ordering)', 'AxesHandle', gca,'AxesLimits', repmat([1; 150], [1,12]), 'AxesLabels', 'none', 'Direction', 'counterclockwise', 'FillOption', 'on', 'AxesDisplay','none', 'Marker', 'none', 'AxesColor',[1 1 1])
+% spider_plot(color_ix(ordering)', 'AxesHandle', gca,'AxesLimits', repmat([1; 150], [1,12]), 'AxesLabels', 'none', 'Direction', 'counterclockwise', 'FillOption', 'on', 'AxesDisplay','none', 'Marker', 'none', 'AxesColor',[1 1 1])
+spider_plot(color_ix(ordering)', 'AxesHandle', gca, 'AxesLabels', 'none', 'Direction', 'counterclockwise', 'FillOption', 'on', 'AxesDisplay','none', 'Marker', 'none', 'AxesColor',[1 1 1])
