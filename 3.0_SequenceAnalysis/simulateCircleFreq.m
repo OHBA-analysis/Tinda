c323328e7a36d08054aa38f0ee3970a25c2a645d
matsvanes <mats.vanes@psych.ox.ac.uk>
2023-02-06 10:49:21 +0000

    play around with freq analysis on the circleplot

--- 3.0_SequenceAnalysis/simulateCircleFreq.m
+++ 3.0_SequenceAnalysis/simulateCircleFreq.m
@@ -30,3 +30,59 @@ f2=ft_freqanalysis(cfg2, f);
 
 figure; plot(f2.freq,f2.powspctrm(fsel,:));
 
+
+
+
+%%
+
+
+
+dat=[];
+dat.label{1} = 'tmp';
+dat.dimord = '{rpt}_chan_time';
+dat.fsample=250;
+for k=1:55
+  vpos=nan(length(vpath{k}),1);
+for ik=1:12
+  vpos(vpath{k}==ik) = disttoplot_manual(ik);
+end
+  dat.trial{k}(1,:) = vpos;
+  dat.time{k} = 1/250:1/250:length(vpath{k})/250;
+end
+
+cfg=[];
+cfg.taper='hanning';
+cfg.method = 'mtmfft';
+cfg.output='pow';
+cfg.foi = 0.01:0.01:10;
+cfg.keeptrials = 'yes';
+f = ft_freqanalysis(cfg, dat)
+
+for k=1:55
+    vpath_sim{k} = simulateVpath(vpath{k},hmmT{k},K);
+end
+
+for l=1:10
+for k=1:length(vpath)
+  vpath_perm{k} = zeros(size(vpath{k}));
+  perm_ordering = randperm(K);
+  for i=1:K
+    vpath_perm{k}(vpath{k}==i)=perm_ordering(i);
+  end
+end
+
+datperm{l}=[];
+datperm{l}.label{1} = 'tmp';
+datperm{l}.dimord = '{rpt}_chan_time';
+datperm{l}.fsample=250;
+for k=1:55
+  vpos=nan(length(vpath_perm{k}),1);
+for ik=1:12
+  vpos(vpath_perm{k}==ik) = disttoplot_manual(ik);
+end
+  datperm{l}.trial{k}(1,:) = vpos;
+  datperm{l}.time{k} = 1/250:1/250:length(vpath{k})/250;
+end
+fperm{l} = ft_freqanalysis(cfg, datperm{l});
+end
+  
