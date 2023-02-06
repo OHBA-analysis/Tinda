201348145435766d9238a94cc1fbb0ce443c6e5a
matsvanes <mats.vanes@psych.ox.ac.uk>
2023-02-06 10:58:58 +0000

    deal with nans

--- utils/compute_rotational_momentum.m
+++ utils/compute_rotational_momentum.m
@@ -1,12 +1,16 @@
-function rotational_momentum = compute_rotational_momentum(angleplot, FO_assym, whichstate)
+function [rotational_momentum, stat] = compute_rotational_momentum(angleplot, FO_assym, whichstate)
 tmp = angleplot.*FO_assym;
+nSj = size(FO_assym,3);
 if exist('whichstate','var') && ~isempty(whichstate)
   % Note that we are counting each (i,j) double because for the rotational
   % momentum per state we take into account (i,j) and (j,i) for all j and one
   % particular i.
   tmp = squeeze(tmp(whichstate,:,:)) + squeeze(tmp(:,whichstate,:));
-  rotational_momentum = imag(sum(tmp));
+  rotational_momentum = imag(nansum(tmp));
 else
-  rotational_momentum = imag(sum(sum(tmp)));
+  rotational_momentum = imag(nansum(nansum(tmp)));
 end
 rotational_momentum = squeeze(rotational_momentum);
+if size(rotational_momentum,2) > size(rotational_momentum,1) && size(rotational_momentum,1)==1
+  rotational_momentum = rotational_momentum';
+end
