697334ef964496d7180299862071dbc45c9c057a
matsvanes <mats.vanes@psych.ox.ac.uk>
2023-03-15 11:05:44 +0000

    add mean to regressors

--- utils/regress_out.m
+++ utils/regress_out.m
@@ -1,5 +1,12 @@
 function data_corrected = regress_out(data, regressors)
-
-beta = regressors\data; % B = X\Y
-model = regressors * beta;
-data_corrected =  data - model;
+if ~exist('regressors', 'var') || isempty(regressors)
+    warning('There were not regressors. data_out = data_in')
+    data_corrected = data;
+else
+    if ~all(regressors(:,1)==1)
+        regressors = [ones(size(regressors,1),1), regressors];
+    end
+    beta = regressors\data; % B = X\Y
+    model = regressors * beta;
+    data_corrected =  data - model;
+end
