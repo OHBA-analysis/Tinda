7e70b19ada0984cda73889e7394b83e62d50a7cc
matsvanes <mats.vanes@psych.ox.ac.uk>
2023-03-15 11:11:11 +0000

    add option to group data from a single vector

--- utils/boxplot_with_scatter.m
+++ utils/boxplot_with_scatter.m
@@ -1,22 +1,39 @@
-function boxplot_with_scatter(data, facecolor, facealpha)
+function boxplot_with_scatter(data, facecolor, facealpha, group)
 
 if ~exist('facealpha', 'var') || isempty(facealpha)
-  facealpha=0.6;
+    facealpha=0.6;
 end
 
 [n, m] = size(data);
 hold on
-for k=1:m
-  if ~exist('facecolor', 'var') || isempty(facecolor)
-    scatter(1+ones(size(data(:,k))).*((k-1)+(rand(size(data(:,k)))-0.5)/2),data(:,k),'filled', 'MarkerFaceAlpha',facealpha)
-  else
-    if iscell(facecolor)
-      MarkerFaceColor = facecolor{k};
+if m==1
+    if exist('group', 'var') || isempty(group)
+        ug = unique(group)
+        for k2=1:length(ug)
+            k3=ug(k2)
+            scatter(1+ones(sum(group==k3),1).*((k3-1)+(rand(sum(group==k3),1)-0.5)/2),data(group==k3,1),'filled', 'MarkerFaceAlpha',facealpha)
+        end
     else
-      MarkerFaceColor = facecolor;
+        k=1;scatter(1+ones(size(data(:,k))).*((k-1)+(rand(size(data(:,k)))-0.5)/2),data(:,k),'filled', 'MarkerFaceAlpha',facealpha)
     end
-    scatter(1+ones(size(data(:,k))).*((k-1)+(rand(size(data(:,k)))-0.5)/2),data(:,k),'filled', 'MarkerFaceAlpha',facealpha, 'MarkerFaceColor',MarkerFaceColor)
-  end
+else
+    for k=1:m
+        if ~exist('facecolor', 'var') || isempty(facecolor)
+            scatter(1+ones(size(data(:,k))).*((k-1)+(rand(size(data(:,k)))-0.5)/2),data(:,k),'filled', 'MarkerFaceAlpha',facealpha)
+        else
+            if iscell(facecolor)
+                MarkerFaceColor = facecolor{k};
+            else
+                MarkerFaceColor = facecolor;
+            end
+            
+            scatter(1+ones(size(data(:,k))).*((k-1)+(rand(size(data(:,k)))-0.5)/2),data(:,k),'filled', 'MarkerFaceAlpha',facealpha, 'MarkerFaceColor',MarkerFaceColor)
+        end
+    end
 end
-h = boxplot(data, 'Colors', 'k', 'Width', .75, 'whisker', 1000); % put this on top
+if exist('group', 'var') && ~isempty(group)
+    h = boxplot(data, group, 'Colors', 'k', 'Width', .75, 'whisker', 1000); % put this on top
+else
+    h = boxplot(data, 'Colors', 'k', 'Width', .75, 'whisker', 1000); % put this on top
+end
 set(h, {'linew'}, {2})
