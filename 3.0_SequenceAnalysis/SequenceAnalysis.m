8fcdd939a1d70e1229b38b2335eabe17cd32d422
matsvanes <mats.vanes@psych.ox.ac.uk>
2023-06-16 15:13:28 +0100

    add matlab colors variable

--- 3.0_SequenceAnalysis/SequenceAnalysis.m
+++ 3.0_SequenceAnalysis/SequenceAnalysis.m
@@ -7,6 +7,7 @@ config = getStudyDetails(whichstudy);
 % other preliminary setup for plotting etc:
 color_scheme = colorscheme(whichstudy);
 hotcold = cmap_hotcold;
+clr = {[0 0.4470 0.7410], [0.8500 0.3250 0.0980], [0.9290 0.6940 0.1250], [0.4940 0.1840 0.5560], [0.4660 0.6740 0.1880], [0.3010 0.7450 0.9330]};
 
 % include option for simulations here - results to be saved in adjacent
 % folder:
