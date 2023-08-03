93ed60a9341c0edc0d9432c5403f7622f79f4f27
mvanes <mvanes@hbaws45.localdomain>
2023-08-03 09:35:19 +0100

    no hard coded n_states

--- utils/circle_angles.m
+++ utils/circle_angles.m
@@ -1,9 +1,10 @@
 function angleplot = circle_angles(bestseq)
 
 % save each subject's rotational strength by this metric:
-disttoplot_manual = zeros(12,1);
+K=length(bestseq);
+disttoplot_manual = zeros(K,1);
 for i3=1:12
-  disttoplot_manual(bestseq(i3)) = exp(sqrt(-1)*i3/12*2*pi);
+  disttoplot_manual(bestseq(i3)) = exp(sqrt(-1)*i3/K*2*pi);
 end
 angleplot = exp(sqrt(-1)*(angle(disttoplot_manual.')-angle(disttoplot_manual)));
 
